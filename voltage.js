52d1c8cad09ec37b1c8000b4176dc7bdc24eb322
Yash-D-Wanth <yashjaiganesh@gmail.com>
2025-07-05 12:09:27 +0530

    voltage.html file added

---
+++ voltage.js
@@ -0,0 +1,22 @@
+let inputDiv=document.getElementById("inputDiv")
+let submitBtn=document.getElementById("submitBtn")
+let resultDiv=document.getElementById("resultDiv")
+submitBtn.addEventListener('click',function(){
+        let units=inputDiv.value
+        let result=0;
+        if(units<100){
+            resultDiv.textContent=`you have to pay $${result}`
+        }
+        else if(units>100 && units<=200){
+            result=(100*0)+((units-100)*5)
+            resultDiv.textContent=`you have to pay $${result}`
+        }
+        else if(units>200 && units<=400){
+            result=(100*0)+(100*5)+((units-150)*10)
+            resultDiv.textContent=`you have to pay $${result}`
+        }
+        else if(units>400){
+            result=0+(100*5)+(200*10)+((units-400)*20)
+            resultDiv.textContent=`you have to pay $${result}`
+        }
+})
